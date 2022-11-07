92edc902c5cd7bb297a32a4480b5a35005d04a2a
Remi <remi.ios333@gmail.com>
2022-11-07 09:47:55 -0500

    Initial Commit

---
+++ loginScreen/loginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginScreenApp.swift
+//  loginScreen
+//
+//  Created by Remi Varghese on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct loginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
